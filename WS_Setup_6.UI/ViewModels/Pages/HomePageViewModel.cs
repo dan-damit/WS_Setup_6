759f7a0063790294a2278bf7a7b041a62f04e08c
Dan Damit <dan@thedamits.com>
2025-10-02 13:43:08 -0500

    Small update to assembly metadata.

--- WS_Setup_6.UI/ViewModels/Pages/HomePageViewModel.cs
+++ WS_Setup_6.UI/ViewModels/Pages/HomePageViewModel.cs
@@ -16,7 +16,7 @@ namespace WS_Setup_6.UI.ViewModels.Pages
         {
             _mainVm = mainVm;
             WelcomeMessage = "Workstation Onboarding Tool";
-            Instruction = "Version: 6.7.6  |  Build Date: 2025.09.12";
+            Instruction = "Version: 6.7.7  |  Build Date: 2025.10.02";
         }
 
         [ObservableProperty]
